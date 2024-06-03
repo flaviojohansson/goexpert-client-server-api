62a4bb079a38babbfe171557f59147de5a113b30
Flavio Johansson <flaviodaniel@outlook.com>
2024-06-03 10:52:02 -0300

    Adicionar contexto para o request do cotacaoHandler

--- server/main.go
+++ server/main.go
@@ -54,75 +54,83 @@ func cotacaoHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.Method, r.RemoteAddr, r.RequestURI)
 
 	const URL = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-
-	// TODO: ctx := r.Context()
-	// e toda a chamada pra cotacao dentro de go routive.
-	// criar channel (bool  mesmo)
-	// no fim do request, channel<-true
-	// select {
-	// case <-chan
-	//     log.Println("Request processada com sucesso")
-	//     w.Write([]byte("Request processada com sucesso"))
-	// case <-ctx.Done():
-	//     log.Println("Request cancelada pelo cliente")
-	// }
-
-	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
-	defer cancel()
-
-	req, err := http.NewRequestWithContext(ctx, "GET", URL, nil)
-	if err != nil {
-		log.Panicln(err)
-	}
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Printf("Request error: %s %v\n", URL, err)
-
-		var statusCode int
-
-		// Existe algum outro tipo de erro aqui que não seja timeout e que eu precise tratar ?
-		if ctx.Err().Error() == "context deadline exceeded" {
-			statusCode = http.StatusGatewayTimeout
-		} else {
-			statusCode = http.StatusInternalServerError
-		}
-		w.WriteHeader(statusCode)
-		return
-	}
-
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		log.Printf("Request error: %s %s\n", URL, res.Status)
-		// Qualquer erro com a API retornaremos erro 500
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Printf("Reading response body error:  %v\n", err)
-	}
-
-	var p fastjson.Parser
-	v, err := p.Parse(string(body))
-	if err != nil {
-		log.Printf("Parsing JSON error: %v\n", err)
-	}
-
-	cotacaoJSON := v.Get("USDBRL").String()
+	const API_DEADLINE = 200 //ms
+	// const DB_DEADLINE = 20 //ms
 
 	var cotacao common.Cotacao
-	if err = json.Unmarshal([]byte(cotacaoJSON), &cotacao); err != nil {
-		log.Panicf("Parsing JSON error %v\n", err)
+	mainChan := make(chan bool)
+	mainContext := r.Context()
+
+	go func() {
+
+		ctx, cancel := context.WithTimeout(context.Background(), API_DEADLINE*time.Millisecond)
+		defer cancel()
+
+		defer func() { mainChan <- false }()
+
+		req, err := http.NewRequestWithContext(ctx, "GET", URL, nil)
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Printf("Request error: %s %v\n", URL, err)
+
+			var statusCode int
+
+			// Existe algum outro tipo de erro aqui que não seja timeout e que eu precise tratar ?
+			if ctx.Err().Error() == "context deadline exceeded" {
+				statusCode = http.StatusGatewayTimeout
+			} else {
+				statusCode = http.StatusInternalServerError
+			}
+			w.WriteHeader(statusCode)
+			return
+		}
+
+		defer res.Body.Close()
+
+		if res.StatusCode != 200 {
+			log.Printf("Request error: %s %s\n", URL, res.Status)
+			// Qualquer erro com a API retornaremos erro 500
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Printf("Reading response body error:  %v\n", err)
+		}
+
+		var p fastjson.Parser
+		v, err := p.Parse(string(body))
+		if err != nil {
+			log.Printf("Parsing JSON error: %v\n", err)
+		}
+
+		cotacaoJSON := v.Get("USDBRL").String()
+
+		if err = json.Unmarshal([]byte(cotacaoJSON), &cotacao); err != nil {
+			log.Panicf("Parsing JSON error %v\n", err)
+		}
+
+		mainChan <- true
+
+		// TODO Gravar em banco de dados
+		// contect timeout: 10 ms
+
+	}()
+
+	select {
+	case ok := <-mainChan:
+		if ok {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(cotacao)
+		}
+	case <-mainContext.Done():
+		log.Println("Connection closed by remote host")
 	}
-
-	// TODO Gravar em banco de dados
-	// contect timeout: 10 ms
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(cotacao)
 
 }
